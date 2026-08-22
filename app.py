238605b0586cb12cd1da67d4e8fa79bb0f3b6031
Norah-prog <lznn333@gmail.com>
2026-08-22 09:14:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(page_title="نور", page_icon="✨")
+
+st.title("أهلاً بك في مساعد نور ✨")
+st.write("أنا هنا لأساعدك")
+
+سؤال = st.text_input("اكتبي سؤالك:")
+
+if سؤال:
+    st.success("سؤالك: " + سؤال)
